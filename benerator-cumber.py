319f7879fca998235c79ccd35d4a981253a7d0c8
Marcin Sedlak <fdmarcin@gmail.com>
2017-10-16 17:17:00 +0200

    Write the program

--- benerator-cumber.py
+++ benerator-cumber.py
@@ -1,1 +1,19 @@
-# This is the main script body
+# Written for python 3.5.3
+import random
+
+
+endname = ""
+
+# choose first name
+names1 = open('first-names.txt').read().splitlines()
+endname += random.choice(names1)
+open('first-names.txt').close
+
+endname += " "
+
+# choose second name
+names2 = open('last-names.txt').read().splitlines()
+endname += random.choice(names2)
+open('last-names.txt').close
+
+print(endname)
