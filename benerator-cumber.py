518286a3053516e18529901905192931962d5cf2
Marcin Sędłak <marcin.sedlak@dcx.bz>
2017-10-17 10:55:38 +0200

    Move unused wordlists to _arc.

--- benerator-cumber.py
+++ benerator-cumber.py
@@ -5,22 +5,6 @@ import random
 
 endname = ""
 
-# # using .title()
-# # choose first name
-# with open('first-names.txt') as f:
-#     data = f.read().splitlines()
-#     endname += random.choice(data)
-
-# endname += " "
-
-# # choose second name
-# with open('last-names.txt') as f:
-#     data = f.read().splitlines()
-#     endname += random.choice(data)
-
-# print("Hello there, my name is " + endname.title() + ".")
-
-# using .capitalize()
 # choose first name
 with open('first-names.txt') as f:
     data = f.read().splitlines()
