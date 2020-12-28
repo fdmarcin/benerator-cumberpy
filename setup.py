c52937187cbf685cac1c28bf70a439982332668f
Marcin Sędłak-Jakubowski <fdmarcin@gmail.com>
2020-12-28 22:13:31 +0100

    Add Python 3.9 support

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="benerator_cumberpy",
-    version="0.2.1",
+    version="0.2.2",
     description="A Ben Cumberbatch-like name generator",
     long_description=long_description,
     url="https://github.com/fdmarcin/benerator-cumberpy",
-    download_url="https://github.com/fdmarcin/benerator-cumberpy/archive/0.2.1.tar.gz",
+    download_url="https://github.com/fdmarcin/benerator-cumberpy/archive/0.2.2.tar.gz",
     author="Marcin Sędłak-Jakubowski",
     author_email="fdmarcin@gmail.com",
     license="MIT",
@@ -34,6 +34,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords="benedict cumberbatch word generator",
     py_modules=["benerator_cumberpy"],
